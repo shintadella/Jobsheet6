cc148a3370d34462a078de8ca1a07c33ff269782
shintadella <vaizatusdella@gmail.com>
2024-10-04 00:26:20 +0700

    ini tugas della

--- Pemilihan2Percobaan122.java
+++ Pemilihan2Percobaan122.java
@@ -12,6 +12,10 @@ public class Pemilihan2Percobaan122 {
         if ((tahun % 4) == 0 ) {
             if (( tahun % 100) != 0)
                 System.out.println( "Tahun Kabisat");
+            else{
+                System.out.println("bukan tahun kabisat");
+            }
+
             }else{
                 System.out.println("Bukan tahun kabisat");
         }
