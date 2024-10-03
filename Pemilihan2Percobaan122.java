d96f1c9701199b754f7868312d3713c0290f3398
shintadella <vaizatusdella@gmail.com>
2024-10-04 00:20:07 +0700

    ini tugas della

---
+++ Pemilihan2Percobaan122.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan122 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan Tahun: ");
+        tahun = input.nextInt();
+
+        if ((tahun % 4) == 0 ) {
+            if (( tahun % 100) != 0)
+                System.out.println( "Tahun Kabisat");
+            }else{
+                System.out.println("Bukan tahun kabisat");
+        }
+        }
+    
+}
