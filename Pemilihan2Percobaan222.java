5389e6d0cbbef0968fcd81507c8566bb47ac5ec9
shintadella <vaizatusdella@gmail.com>
2024-10-04 15:08:47 +0700

    ini tugas della

--- Pemilihan2Percobaan222.java
+++ Pemilihan2Percobaan222.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pemilihan2Percobaan22 {
+public class Pemilihan2Percobaan222 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
