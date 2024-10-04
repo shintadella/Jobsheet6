63f09a22c720a51aab0ae4a8877f08c55443aba6
shintadella <vaizatusdella@gmail.com>
2024-10-04 11:14:03 +0700

    ini tugas della

--- flowchart2.java
+++ flowchart2.java
@@ -2,7 +2,53 @@ import java.util.Scanner;
 
 public class flowchart2 {
     public static void main(String[] args) {
+        Scanner Absen22 = new Scanner(System.in);
+
+        double hargaKamus = 15000;
+        double hargaNovel = 10000;
+        double hargaBukuLain = 5000;
+
+        System.out.print("Masukkan jenis buku (kamus/novel/lain): ");
+        String jenisBuku = Absen22.nextLine().toLowerCase();
+        System.out.print("Masukkan jumlah buku: ");
+        int jumlahBuku = Absen22.nextInt();
+
+        double totalHarga = 0;
+        double totalDiskon = 0;
+
+        if (jenisBuku.equalsIgnoreCase("kamus")) {
+            double diskon = 0;
+            if (jumlahBuku > 2) {
+                diskon = 0.10 + 0.02;
+            } else {
+                diskon = 0.10; 
+            }
+            totalDiskon = hargaKamus * jumlahBuku * diskon;
+            totalHarga = hargaKamus * jumlahBuku - totalDiskon;
+        } else if (jenisBuku.equalsIgnoreCase("novel")) {
+            double diskon = 0;
+            if (jumlahBuku > 3) {
+                diskon = 0.07 + 0.02; 
+            } else {
+                diskon = 0.07 + 0.01; 
+            }
+            totalDiskon = hargaNovel * jumlahBuku * diskon;
+            totalHarga = hargaNovel * jumlahBuku - totalDiskon;
+        } else {
+            double diskon = 0;
+            if (jumlahBuku > 3) {
+                diskon = 0.05; 
+            } else {
+                diskon = 0; 
+            }
+            totalDiskon = hargaBukuLain * jumlahBuku * diskon;
+            totalHarga = hargaBukuLain * jumlahBuku - totalDiskon;
+        }
+
+        System.out.println("Total diskon yang diterima: " + totalDiskon);
+        System.out.println("Total harga untuk " + jumlahBuku + " buku " + jenisBuku + ": " + totalHarga);
         
+
+        Absen22.close();;
     }
-    
 }
---
+++ flowchart3.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class flowchart3 {
+    public static void main(String[] args) {
+        Scanner Absen22 = new Scanner (System.in);
+
+        String merk,kategori;
+        double ukuran,harga=0;
+
+        System.out.println("Daftar Harga Toko Sepatu");
+        System.out.println("Merk Sepatu :");
+        System.out.println("1. Converse ");
+        System.out.println("2. Sketcher ");
+        System.out.println("3. Nike ");
+        System.out.println("Pilih merk Sepatu apa yang di beli : ");
+        merk = Absen22.nextLine();
+        System.out.println("Berapa ukuran sepatu anda : ");
+        ukuran = Absen22.nextDouble();
+        System.out.println("--------------------------------------------");
+
+        if (merk.equalsIgnoreCase("Converse")) {
+            if (ukuran >= 36 && ukuran<=40) {
+                harga = 800000;
+                System.out.println("Kategori sepatu Slip On dengan harga Rp "+ harga);
+            }
+            else if (ukuran >= 40 && ukuran<=44) {
+                harga = 1200000;
+                System.out.println("Kategori sepatu High Top dengan harga Rp "+ harga);
+            }else {
+                System.out.println("Ukuran sepatu anda tidak tersedia!");
+            }
+        } else if (merk.equalsIgnoreCase("Skatcher")) {
+            if (ukuran >= 36 && ukuran<=40) {
+                harga = 1000000;
+                System.out.println("Kategori sepatu untuk Wanita/woman dengan harga Rp "+ harga);
+            }
+            else if (ukuran >= 41 && ukuran<=44) {
+                harga = 180000;
+                System.out.println("Kategori sepatu untuk Pri/man dengan harga Rp "+ harga);
+            }else {
+                System.out.println("Ukuran sepatu anda tidak tersedia!");
+            }
+        } else if (merk.equalsIgnoreCase("Nike")) {
+            if (ukuran >= 36 && ukuran<=40) {
+                harga = 750000;
+                System.out.println("Kategori sepatu untuk anak-anak/Kids dengan harga Rp "+ harga);
+            }
+            else if (ukuran >= 40 && ukuran<=44) {
+                harga = 1500000;
+                System.out.println("Kategori sepatu untuk Dewasa/Adult dengan harga Rp "+ harga);
+            }else {
+                System.out.println("Ukuran sepatu anda tidak tersedia!");
+            }
+        } else{
+            System.out.println("Masukkan merk sepatu yang tersedia!");
+        }
+        Absen22.close();
+    }
+    
+}
